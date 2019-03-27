db8cece8a3bd8059d2c12785de709fcbd39ae3a4
Kenneth Li <likenhp@gmail.com>
2019-03-27 12:54:39 -0700

    completed displaying dummy student data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: "David Lee",
+        course: "Banging 101",
+        grade: "100",
+        id: 1
+    },
+    {
+        name: "David",
+        course: "Health and Fitness",
+        grade: "90",
+        id: 2
+    },
+    {
+        name: "Kylie",
+        course: "Worldly Wisdom",
+        grade: "100",
+        id: 3
+    },
+    {
+        name: "Quanye West",
+        course: "Kanye West Impersonation",
+        grade: "100",
+        id: 4
+    },
+    {
+        name: "Vivian",
+        course: "Growth",
+        grade: "1",
+        id: 5
+    }
+]
