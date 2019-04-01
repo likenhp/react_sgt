5a7fd6335b52d847e11cbdfba1db4f5c7818d7cb
Kenneth Li <likenhp@gmail.com>
2019-04-01 16:18:34 -0700

    connected backend node server with react sgt used axios to add students used axios to delete students used axios to display students

--- src/components/app.js
+++ src/components/app.js
@@ -2,55 +2,90 @@ import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min";
 import '../assets/css/app.scss';
 import React, {Component} from 'react';
+import axios from "axios";
 import StudentsTable from "./students_table"
 import AddStudent from "./add_student";
-import studentData from "../dummy_data/student_list";
-
-let id = 100;
 
 class App extends Component{
     state = {
-        students: []
+        students: [],
+        error: ""
     }
 
-    addStudent = (student) => {
-        student.id = id++
-        this.setState({
-            students: [...this.state.students, student] 
-        });
+    addStudent = async (student) => {
+        await axios.post("/api/grades", student);
+        this.getStudentData();
+
+        // student.id = id++
+        // this.setState({
+        //     students: [...this.state.students, student] 
+        // });
     }
 
-    deleteStudent = (id) => {
-        const studentsCopy = this.state.students.slice(); //slicing with no value makes a copy of the array
+    deleteStudent = async (id) => {
+        await axios.delete(`/api/grades/${id}`);
+        this.getStudentData();
+        
+        // const studentsCopy = this.state.students.slice(); 
+        //slicing with no value makes a copy of the array
 
-        const index = studentsCopy.findIndex((student)=>{
-            return student.id === id;
-        });
+        // const index = studentsCopy.findIndex((student)=>{
+        //     return student.id === id;
+        // });
 
-        if(index >= 0){
-            studentsCopy.splice(index, 1);
+        // if(index >= 0){
+        //     studentsCopy.splice(index, 1);
 
-            this.setState({
-                students: [...studentsCopy]
-            })
-        }
+        //     this.setState({
+        //         students: [...studentsCopy]
+        //     })
+        // }
     }
 
     componentDidMount(){
         this.getStudentData();
     }
 
-    getStudentData(){
+    async getStudentData(){
         //Call server here
-        this.setState({
-            students: studentData
+
+        try{
+            const resp = await axios.get("/api/grades");
+
+            this.setState({
+                students: resp.data.data
+            });
+        } catch(err){
+            console.log("Error getting data:", err.message);
+
+            this.setState({
+                error: "Error retrieving student data"
+            });
+        }
+
+
+        /* Axios method
+        axios.get('http://localhost:3001/api/gradesd').then((resp) =>{
+            console.log("Server Response", resp);
+
+            this.setState({
+                students: resp.data.data
+            });
+        }).catch((err)=>{
+            console.log("Server Error with getting Server data", err);
+
+            this.setState({
+                error: "Error retrieving student data"
+            })
         });
+        */
     }
 
     render(){
         return(
             <div>
                 <h1 className="center"> React SGT</h1>
+                <h5 className="red-text text-darken-4">{this.state.error}</h5>
                 <div className="row">
                     <StudentsTable col="s12 m8" delete={this.deleteStudent} list={this.state.students}/>
                     <AddStudent col="s12 m4" add={this.addStudent}/>
